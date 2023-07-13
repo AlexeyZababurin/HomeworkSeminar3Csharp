391ebaf8fc2ca7501630f20b225c70b18f4e8448
Alexey <Lehan347@yandex.ru>
2023-07-13 19:51:03 +0300

    The program was written for task 23

---
+++ task23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт 
+// таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Sqrt (int n)
+{
+    Console.WriteLine($"Таблица квадратов числа от 1 до {n}:", n);
+    for (int i = 1; i < n; i++)
+    {
+        Console.Write(i*i*i+", ");
+    }
+    Console.Write(n*n*n);
+}
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+n = Math.Abs(n);
+Sqrt(n);
